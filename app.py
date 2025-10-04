2b8ded966020c14cf967ebddc902d2181a03ce22
yess1ne <Mohamedyessine.Elichi@esprit.tn>
2025-10-04 20:56:32 +0100

    intégration du modéle Kmeans 5credit goes to Maissamk and eyalatiri

--- app.py
+++ app.py
@@ -11,10 +11,17 @@ artifact = joblib.load(MODEL_PATH)
 model = artifact["model"]
 scaler = artifact["scaler"]
 features = artifact["features"]
+df_clustering = pd.read_csv("datadelevry_cleaned.csv")
+clustering_model= joblib.load("clustering.pkl")
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+@app.route("/clustering_index", methods=["GET"])
+def clustering_index():
+    stats = df_clustering.describe().to_html(classes='table table-striped', border=0)
+    return render_template('clustering_index.html', tables=stats)
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -44,6 +51,34 @@ def predict():
     except Exception as e:
         return f"Unexpected error: {str(e)}", 500
 
+@app.route('/predict_clustering', methods=['GET', 'POST'])
+def predict_clustering():
+    if request.method == 'POST':
+        # Récupérer les valeurs du formulaire
+        try:
+            input_data = {
+    'total_orders': float(request.form['total_orders']),
+    'avg_order_value': float(request.form['avg_order_value']),
+    'avg_items_per_order': float(request.form['avg_items_per_order']),
+    'avg_delivery_time': float(request.form['avg_delivery_time']),
+    'driver_utilization_rate': float(request.form['driver_utilization_rate']),
+    'price_range': float(request.form['price_range']),
+    'revenue_per_item': float(request.form['revenue_per_item']),
+    'delivery_time_consistency': float(request.form['delivery_time_consistency']),
+    'operational_efficiency': float(request.form['operational_efficiency'])
+}
+
+            # Transformer en DataFrame
+            X_new = pd.DataFrame([input_data])
+
+            # Prédire le cluster
+            cluster = clustering_model.predict(X_new)[0]
+            return render_template('clustering_result.html', cluster=cluster, data=input_data)
+
+        except Exception as e:
+            return f"Erreur lors de la prédiction : {e}"
+
+    return render_template('clustering_predict.html')
 
 @app.route("/form", methods=["GET"])
 def form():
